bbc4c0c0447a9c59af3165a6d378771313a0be16
Sankarshan Acharya <sankarshansacharya@yahoo.com>
2018-05-06 18:26:54 -0700

    This is the version of the scraper that is ready for testing

--- tests/scraperapp/scraperapp/settings.py
+++ tests/scraperapp/scraperapp/settings.py
@@ -14,9 +14,35 @@ BOT_NAME = 'scraperapp'
 SPIDER_MODULES = ['scraperapp.spiders']
 NEWSPIDER_MODULE = 'scraperapp.spiders'
 
+#
+# Splash config (https://github.com/scrapy-plugins/scrapy-splash)
+#
+
+# Add the Splash server address
+SPLASH_URL = 'http://localhost:8050'
+
+# Enable the Splash middleware by adding it and changing HttpCompressionMiddleware priority
+# Order 723 is just before HttpProxyMiddleware (750) in default scrapy settings.
+# HttpCompressionMiddleware priority should be changed in order to allow advanced response processing;
+# see https://github.com/scrapy/scrapy/issues/1895 for details
+#DOWNLOADER_MIDDLEWARES = {
+#    'scraperapp.middlewares.MyCustomDownloaderMiddleware': 543,
+#}
+
+# These 2 lines are necessary because Scrapy doesn't provide a way to override request fingerprints calculation
+# algorithm globally; this could change in future.
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+
+
+
+#
+# Scraper config
+#
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'scraperapp (+http://www.yourdomain.com)'
+USER_AGENT = 'scraperapp (+https://www.pdefusco.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -38,6 +64,10 @@ ROBOTSTXT_OBEY = True
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
+
+# Enable or disable downloader middlewares
+# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
+
 # Override the default request headers:
 #DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -50,11 +80,6 @@ ROBOTSTXT_OBEY = True
 #    'scraperapp.middlewares.ScraperappSpiderMiddleware': 543,
 #}
 
-# Enable or disable downloader middlewares
-# See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'scraperapp.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
@@ -64,22 +89,61 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'scraperapp.pipelines.ScraperappPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'scraperapp.pipelines.ScraperappPipeline': 300,
+}
+
+FIELDS_TO_EXPORT = [
+    'name',
+    'price',
+    'nightly_price',
+    #    'calendar_updated_at',
+    #    'min_nights',
+    #    'url',
+    'summary',
+    'description',
+    'space',
+    'satisfaction_guest',
+    'accuracy_rating',
+    #    'amenities',
+    'access',
+    'house_rules',
+    'response_rate',
+    'response_time',
+    'notes',
+    #    'cancel_policy',
+    #    'host_id',
+    #    'hosting_id',
+    #    'instant_book',
+    #    'interaction',
+    'neighborhood_overview',
+    #    'rating_checkin',
+    #    'rating_cleanliness',
+    #    'rating_communication',
+    #    'review_count',
+    #    'reviews',
+    #    'room_type',
+    #    'person_capacity',
+    #    'transit',
+    #    'bed_type',
+]
+
+MINIMUM_MONTHLY_DISCOUNT = 0  # percent
+MINIMUM_WEEKLY_DISCOUNT = 0  # percent
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
